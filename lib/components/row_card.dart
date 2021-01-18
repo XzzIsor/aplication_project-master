3ad1efb9790b47c7b4d8b73c937b74451e9b0142
Esteban R <70593544+XzzIsor@users.noreply.github.com>
2021-01-18 03:17:00 -0500

    Update3

--- lib/components/row_card.dart
+++ lib/components/row_card.dart
@@ -1,30 +1,58 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 
 class RowCard extends StatelessWidget {
-  RowCard(this._title, this._subtitle, this._icon);
+  RowCard(this._title, this._subtitle, this._icon, this._image);
 
   final String _title;
   final String _subtitle;
   final IconData _icon;
+  final String _image;
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListBody(
-      mainAxis: Axis.horizontal,
-      children: [
-        Container(
-          child: ListTile(
-            leading: Icon(_icon),
-            title: Text(_title),
-            subtitle: Text(_subtitle),
-          ),
-          width: 200.0,
-          decoration: BoxDecoration(color: Colors.deepPurpleAccent),
-        )
-      ],
-     ),
-    elevation: 15.0,
+        mainAxis: Axis.horizontal,
+        children: [
+          Column(
+            children: [
+              Container(
+            child: ListTile(
+              leading: Icon(_icon),
+              title: Text(_title),
+              subtitle: Text(_subtitle),
+              onTap: () {},
+            ),
+            width: 250.0,
+            decoration: BoxDecoration(
+              color: Colors.deepPurpleAccent, 
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(25.0),
+              )
+            ),
+            ),
+           Container(
+             child: Text(''),
+             decoration: BoxDecoration(
+               image: DecorationImage(
+                 image: ExactAssetImage(_image), 
+                 fit: BoxFit.fill
+               ),
+               borderRadius: BorderRadius.vertical(
+                 bottom: Radius.circular(25.0),
+               )
+             ),
+             width: 250.0,
+             height: 176.0,
+           ),
+            ],
+          )
+        ],
+      ),
+      elevation: 15.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0)),
     );
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -11,16 +11,16 @@ class HomePage extends StatelessWidget {
           backgroundColor: Colors.deepPurpleAccent,
         ),
         body: ListView(
-         padding: EdgeInsets.only(bottom: 500.0, top: 5.0, left: 5.0, right: 5.0),
+         padding: EdgeInsets.only(bottom: 420.0, top: 5.0, left: 5.0, right: 5.0),
           scrollDirection: Axis.horizontal, 
           children: [
-          RowCard('Titulo1', 'Subtitulo1', Icons.ac_unit),
-          RowCard('Titulo2', 'Subtitulo2', Icons.access_alarms),
-          RowCard('Titulo3', 'Subtitulo3', Icons.accessible_forward),
-          RowCard('Titulo3', 'Subtitulo3', Icons.add_a_photo),
+          RowCard('Titulo1', 'Subtitulo1', Icons.ac_unit,'assets/images/chiguiro.jpg'),
+          RowCard('Titulo2', 'Subtitulo2', Icons.access_alarms,'assets/images/Agua.jpg'),
+          RowCard('Titulo3', 'Subtitulo3', Icons.accessible_forward,'assets/images/Agua 2.jpg'),
+          RowCard('Titulo4', 'Subtitulo4', Icons.add_a_photo,'assets/images/Paisaje.jpg'),
+          RowCard('Titulo5', 'Subtitulo5', Icons.access_alarms_rounded,'assets/images/Paisaje2.jpg'),
         ]
-       )
-       
+       ),
       );
   }
 }
